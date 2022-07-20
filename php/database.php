2daca307e1d98b8d6e0254352e4157ef86f2459b
agusgrance <agustingran@gmail.com>
2022-07-20 15:29:32 -0300

    Change User

--- php/database.php
+++ php/database.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=UTF-8");
 $db_host = 'localhost';
-$db_username = 'root';
-$db_password = '';
+$db_username = 'final';
+$db_password = 'final';
 $db_name = 'final_progra_web';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 
