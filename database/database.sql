4e8952b9f310066833af2bc7f755ea8a3df22ada
agusgrance <agustingran@gmail.com>
2023-03-01 15:26:20 -0300

    update database

--- database/database.sql
+++ database/database.sql
@@ -3,11 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-07-2022 a las 19:43:07
+-- Tiempo de generación: 01-03-2023 a las 19:25:43
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -21,8 +20,107 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `final_progra_web`
 --
-CREATE DATABASE IF NOT EXISTS `final_progra_web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `final_progra_web`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `chat`
+--
+
+CREATE TABLE `chat` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `chat`
+--
+
+INSERT INTO `chat` (`id`, `sender_id`, `receiver_id`, `message`, `timestamp`) VALUES
+(1, 15, 18, 'prueba', '2023-02-28 02:28:23'),
+(3, 15, 18, 'hola!', '2023-02-28 02:57:17'),
+(4, 18, 15, 'hola amigo', '2023-02-28 02:57:19'),
+(5, 18, 15, 'como estas?', '2023-02-28 22:16:31'),
+(6, 18, 15, 'buenasss', '2023-02-28 22:45:30'),
+(7, 18, 15, 'asdasd', '2023-02-28 22:45:58'),
+(8, 18, 18, 'alo', '2023-02-28 22:46:07'),
+(9, 18, 19, 'holaaa', '2023-02-28 22:46:11'),
+(10, 19, 18, 'como estas?', '2023-02-28 22:46:44'),
+(11, 19, 15, 'hola sir', '2023-02-28 22:46:54'),
+(12, 18, 19, 'bien y vos', '2023-02-28 23:07:23'),
+(13, 19, 18, 'asdasd', '2023-02-28 23:32:21'),
+(14, 19, 18, 'sadasdasdasd', '2023-02-28 23:32:23'),
+(18, 18, 19, 'http://localhost:4200/post/40', '2023-03-01 14:47:16'),
+(19, 18, 18, 'http://localhost:4200/post/40', '2023-03-01 14:47:21'),
+(20, 18, 15, 'http://localhost:4200/post/40', '2023-03-01 14:47:22'),
+(21, 18, 15, 'http://localhost:4200/post/40', '2023-03-01 14:50:04'),
+(22, 18, 18, 'http://localhost:4200/post/40', '2023-03-01 14:50:07'),
+(23, 18, 15, 'http://localhost:4200//dashboard', '2023-03-01 15:18:39'),
+(24, 18, 15, '/post/40', '2023-03-01 15:22:16'),
+(25, 18, 15, 'http://localhost:420040', '2023-03-01 15:23:17'),
+(26, 18, 15, 'http://localhost:4200/40', '2023-03-01 15:23:38'),
+(27, 18, 15, 'http://localhost:4200/post/40', '2023-03-01 15:23:57'),
+(28, 18, 19, 'http://localhost:4200/post/40', '2023-03-01 15:24:08'),
+(29, 18, 15, 'http://localhost:4200/post/42', '2023-03-01 15:32:02'),
+(30, 18, 19, 'http://localhost:4200/post/42', '2023-03-01 15:34:29'),
+(31, 18, 19, 'http://localhost:4200/post/40', '2023-03-01 15:40:19'),
+(32, 18, 15, 'https://bookshelfapp.000webhostapp.com//post/43', '2023-03-01 16:06:07'),
+(33, 18, 18, 'https://bookshelfapp.000webhostapp.com//post/40', '2023-03-01 16:06:26'),
+(34, 18, 18, 'https://bookshelfapp.000webhostapp.com//post/42', '2023-03-01 16:06:43'),
+(35, 18, 18, 'http://localhost:4200/post/42', '2023-03-01 16:14:20');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL,
+  `posteo_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `texto` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `posteo_id`, `user_id`, `texto`) VALUES
+(1, 40, 18, 'sadasd'),
+(5, 42, 18, 'asdasdasd'),
+(6, 40, 18, 'asdasdfasfasfasf');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `posteo_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `likes`
+--
+
+INSERT INTO `likes` (`id`, `posteo_id`, `user_id`) VALUES
+(2, 41, 18),
+(3, 42, 19),
+(5, 40, 19),
+(6, 41, 19),
+(7, 43, 19),
+(8, 42, 18),
+(9, 43, 18),
+(11, 40, 18),
+(15, 40, 23),
+(16, 44, 23);
 
 -- --------------------------------------------------------
 
@@ -35,18 +133,17 @@ CREATE TABLE `posteos` (
   `user_id` int(11) NOT NULL,
   `text` varchar(250) NOT NULL,
   `img` varchar(250) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `isbn` varchar(255) NOT NULL,
+  `rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `posteos`
 --
 
-INSERT INTO `posteos` (`id`, `user_id`, `text`, `img`, `date`) VALUES
-(36, 16, 'tremendo meme', '../../assets/uploads/test.jpg', '2022-07-20'),
-(37, 17, 'wey yaaaa', '../../assets/uploads/descarga.jpg', '2022-07-20'),
-(38, 15, 'JAAAAA', '../../assets/uploads/test.webp', '2022-07-20'),
-(39, 16, 'espera un momento...', '../../assets/uploads/tareaencasa.jpg', '2022-07-20');
+INSERT INTO `posteos` (`id`, `user_id`, `text`, `img`, `date`, `isbn`, `rating`) VALUES
+(44, 18, 'Muy buen libro', '../../assets/uploads/imageFigma.jpg', '2023-03-01', '1668001225', 3);
 
 -- --------------------------------------------------------
 
@@ -78,21 +175,40 @@ CREATE TABLE `users` (
   `name` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `rol` int(11) DEFAULT NULL
+  `rol` int(11) DEFAULT NULL,
+  `avatar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `password`, `email`, `rol`) VALUES
-(15, 'uces', 'uces', 'uces', 1),
-(16, 'user', 'user', 'user', 2),
-(17, 'pepe', 'pepe', 'pepe', 2);
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `rol`, `avatar`) VALUES
+(15, 'uces', '32431e7b90a7d8366d213d5895862c94', 'uces', 1, 'https://avatars.dicebear.com/api/initials/uc.svg'),
+(18, 'agus', '071e476f6fd56aaae264fc9c6725feaa', 'agustingran@gmail.com', 2, 'https://avatars.dicebear.com/api/initials/ag.svg'),
+(20, 'pedro', 'c6cc8094c2dc07b700ffcc36d64e2138', 'pedro', 2, 'https://avatars.dicebear.com/api/initials/pe.svg');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `posteos`
@@ -117,10 +233,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT de la tabla `posteos`
 --
 ALTER TABLE `posteos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -132,12 +266,9 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-SET FOREIGN_KEY_CHECKS=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-GRANT ALL PRIVILEGES ON final_progra_web.* TO final@localhost IDENTIFIED BY 'final';
